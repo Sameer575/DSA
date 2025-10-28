405c9897a1da9e1ba06e220b039bf3868a3991b4
Sameer <168402399+Sameer575@users.noreply.github.com>
2025-10-28 15:31:43 +0530

    Time: 7 ms (0.74%) | Memory: 46.6 MB (9.02%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int st=0;
-        int lt=nums.size()-1;
-        while(lt>=st){
-            int mid=st+(lt-st)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            if (nums[mid]<target){st=mid+1;}
-            if(nums[mid]>target) {lt=mid-1;}
-
+    int bs(vector<int>nums,int target,int st,int end){
+        if(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>target) return bs(nums,target,st,mid-1);
+            if(nums[mid]<target) return bs(nums,target,mid+1,end);
         }
         return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        return bs(nums,target,0,nums.size()-1);
+        // int st=0;
+        // int lt=nums.size()-1;
+        // while(lt>=st){
+        //     int mid=st+(lt-st)/2;
+        //     if(nums[mid]==target){
+        //         return mid;
+        //     }
+        //     if (nums[mid]<target){st=mid+1;}
+        //     if(nums[mid]>target) {lt=mid-1;}
+
+        // }
+        // return -1;
     }
 };
