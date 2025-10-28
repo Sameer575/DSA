639735e001c651e300e78c794180459717b5411a
Sameer <168402399+Sameer575@users.noreply.github.com>
2025-10-28 13:11:37 +0530

    Time: 4 ms (23.44%) | Memory: 52.3 MB (34.81%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int farthest = nums[0];
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 1; i < nums.size(); i++) {
             if (i > farthest) return false; // can't reach this point
             farthest = max(farthest, i + nums[i]);
             if (farthest >= nums.size() - 1) return true; // can reach end
