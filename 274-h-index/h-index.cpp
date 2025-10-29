cea7ecc59f926569d784714392b3aa0d5d2f871e
Sameer <168402399+Sameer575@users.noreply.github.com>
2025-10-29 14:42:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (64.26%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -3,13 +3,12 @@ public:
     int hIndex(vector<int>& citations) {
         int n=citations.size();
         sort(citations.begin(),citations.end(), greater<int>());
-        int h=0;
         for (int i=0;i<n;++i) {
             if (citations[i]>=i+1)
-                h=i+1;
+                continue;
             else
-                break;
+                return i;
         }
-        return h;
+        return n;
     }
 };
