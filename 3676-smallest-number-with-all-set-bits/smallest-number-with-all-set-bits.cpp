7c19c3c44008be1d689ed96fb5e7a9e2f811c9cb
Sameer <168402399+Sameer575@users.noreply.github.com>
2025-10-29 12:13:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (60.80%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int smallestNumber(unsigned n) {
-        return bit_ceil(n+1)-1; 
-    // int smallestNumber(int n) {
-        // int ans=0;
-        // for(int i=1;i<=n;i++){
-        //     int z=pow(2,i)-1;
-        //     if(z>=n){
-        //     ans+=z;
-        //     break;}
-        // }
-        // return ans;
+    // int smallestNumber(unsigned n) {
+    //     return bit_ceil(n+1)-1; 
+    int smallestNumber(int n) {
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            int z=pow(2,i)-1;
+            if(z>=n){
+            ans+=z;
+            break;}
+        }
+        return ans;
     }
 };
